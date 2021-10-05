0979b74c66688b4830390be90f1794e05ea12344
Allan Verde <allangreen2906@gmail.com>
2021-10-05 16:44:10 -0300

    Resolvendo conflitos

--- asset/index.js
+++ asset/index.js
@@ -36,6 +36,61 @@
 
 
 
+//FOSTER
+
+
+for (let i = 0; i <= 6; i ++){
+    let tabela = document.getElementById('tabelaJogo')
+    let addDiv = document.createElement('div')
+    tabela.appendChild(addDiv);
+    addDiv.classList.add('coluna')
+}
+
+let colunas = document.querySelectorAll('.coluna')
+
+colunas.forEach(coluna => {
+    coluna.addEventListener("click", addDisco)
+});
+
+function addDisco(){
+    console.log("bla")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ROMULO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // ALLAN
 
